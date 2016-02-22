f6acd1d17a100b3bf79dacba99c939b0b607de61
Gelton Cruz <gelton@Midiaplural.com.br>
2016-02-22 16:07:06 -0300

    Create Pessoa.java

---
+++ helloworld/Pessoa.java
@@ -0,0 +1,15 @@
+public class Pessoa{
+    public String nome;
+    private float peso; 
+    
+    
+    public float getPeso(){
+        return this.peso;
+    }
+    
+    public void setPeso(float novoPeso){
+        this.peso = novoPeso;
+    }
+    
+    
+}
